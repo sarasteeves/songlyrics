8bb547e0cea6c6fa0729582df0fa24865fb516f4
sarasteeves <sarasteeves@gmail.com>
2017-01-08 19:01:34 +0000

    Corrected typos and added clearer comments

--- language_model.py
+++ language_model.py
@@ -13,7 +13,7 @@ from numpy.random import rand
 class languageModel(object):
     def __init__(self, order=1):
         self._characters = []
-        self._occurences = defaultdict(int)
+        self._occurrences = defaultdict(int)
         self._order = order
     
     # TRAINING
@@ -22,19 +22,21 @@ class languageModel(object):
         for i in list(set(text)):
             if i not in self._characters:
                 self._characters.append(i)
-        # record moving window of characters (size order) and the character that follows, together with frequency in _occurences
+        # record moving window of characters (size order) and the character that follows, together with frequency in _occurrences
         for i in range(len(text)-self._order):
-            self._occurences[text[i:i+self._order], text[i+self._order]] += 1
+            self._occurrences[text[i:i+self._order], text[i+self._order]] += 1
     
     # Get occurences of character set from _occurrences and find weighted probabilities
     def predict(self, character):
-        # WORK OUT WHAT THIS LAST ONE IS
-        probs = [self._occurences[(character, c)] for c in self._characters]
+        # Create list of counts of each 'character set, next character' 
+        probs = [self._occurrences[(character, c)] for c in self._characters]
+        # pick a character based on the weighted probabilities of each character following the character set
         return self._characters[self._weighted_pick(probs)]
 
-    # 
+    # GENERATING NEW TEXT
     def generate(self, start_chars, n):
-        result = start_chars
+        result = start_chars # initialise output with seed text
+        # add new predicted characters to output using the predict function
         for i in range(n):
             new_char = self.predict(start_chars)
             result += new_char
@@ -43,5 +45,6 @@ class languageModel(object):
 
     @staticmethod
     def _weighted_pick(weights):
-        # BREAK DOWN TO MORE LINES FOR CLARITY
-        return searchsorted(cumsum(weights), rand()*sum(weights))
+        countSum = sum(weights) # sum of all 'character set, next character' counts
+        cumulativeSum = cumsum(weights) # cumulative sum of 'character set, next character' counts
+        return searchsorted(cumulativeSum, rand()*countSum) # generate index number for next character using weighted probability
