043b6c67c339a708467f3720e8f3b818164a1d66
sarasteeves <sarasteeves@gmail.com>
2017-01-15 19:54:02 +0000

    Added code to predict by word

--- language_model.py
+++ language_model.py
@@ -6,11 +6,12 @@ Created on Fri Jan 06 18:16:17 2017
 """
 
 from collections import defaultdict
+from nltk import word_tokenize
 from numpy import cumsum, sum, searchsorted
 from numpy.random import rand
 
 # Create class which will have a dictionary of character occurences and list of possible characters
-class languageModel(object):
+class charModel(object):
     def __init__(self, order=1):
         self._characters = []
         self._occurrences = defaultdict(int)
@@ -48,3 +49,41 @@ class languageModel(object):
         countSum = sum(weights) # sum of all 'character set, next character' counts
         cumulativeSum = cumsum(weights) # cumulative sum of 'character set, next character' counts
         return searchsorted(cumulativeSum, rand()*countSum) # generate index number for next character using weighted probability
+
+# Create class equivalent to character class, but for words
+class wordModel(object):
+    def __init__(self):
+        self._words = []
+        self._occurences = defaultdict(int)
+    
+    # TRAINING
+    def train(self, text):
+        # update list of words with new ones seen in training data
+        vocabulary = list(word_tokenize(text)) # create list of words from text
+        for i in list(set(vocabulary)):
+            if i not in self._words:
+                self._words.append(i)
+        # record moving window of bigrams and the word that follows, together with frequency in _occurences
+        for i in range(len(vocabulary)-2):
+            self._occurences[vocabulary[i]+ ' ' + vocabulary[i+1], vocabulary[i+2]] += 1
+    
+    # Get occurences of bigrams from _occurrences and find weighted probabilities for next word
+    def predict(self, bigram):
+        probs = [self._occurences[(bigram, b)] for b in self._words]
+        return self._words[self._weighted_pick(probs)]
+
+    # GENERATING NEW TEXT
+    def generate(self, start_words, n):
+        result = start_words
+        for i in range(n):
+            new = self.predict(start_words)
+            result += ' ' + new
+            words = list(word_tokenize(result))
+            start_words = words[-2] + ' ' + new
+        return result
+
+    @staticmethod
+    def _weighted_pick(weights):
+        countSum = sum(weights)
+        cumulativeSum = cumsum(weights)
+        return searchsorted(cumulativeSum, rand()*countSum)
--- lyric_predictions.py
+++ lyric_predictions.py
@@ -7,9 +7,18 @@ Created on Fri Jan 06 18:31:47 2017
 import pandas as pd
 import language_model as lm
 
-def generate_lyrics(dataset, artist, seed, n):
+def generate_lyrics_char(dataset, artist, seed, n):
     lyrics = dataset.loc[dataset['artist']==artist, 'text'] # get all text for entries with specified artist
-    model = lm.languageModel(order=len(seed))
+    model = lm.charModel(order=len(seed))
+    for song in lyrics:
+        song = song.lower()
+        model.train(song)
+            
+    return model.generate(seed, n)
+
+def generate_lyrics_word(dataset, artist, seed, n):
+    lyrics = dataset.loc[dataset['artist']==artist, 'text'] # get all text for entries with specified artist
+    model = lm.wordModel()
     for song in lyrics:
         song = song.lower()
         model.train(song)
@@ -26,10 +35,17 @@ if __name__=='__main__':
     print data.columns.values
     print data.describe()
     print
-    print 'Generating lyrics for Coldplay...'
-    print generate_lyrics(data, 'Coldplay', "i don'", 200) 
+    print 'Generating lyrics for Coldplay (by character)...'
+    print generate_lyrics_char(data, 'Coldplay', "i don'", 200) 
     print
     print
-    print 'Generating lyrics for Robbie Williams...'
-    print generate_lyrics(data, 'Robbie Williams', "i don'", 200)
-    
+    print 'Generating lyrics for Coldplay (by word)...'
+    print generate_lyrics_word(data, 'Coldplay', "i don", 100) 
+    print
+    print
+    print 'Generating lyrics for Robbie Williams (by character)...'
+    print generate_lyrics_char(data, 'Robbie Williams', "i don'", 200)
+    print
+    print
+    print 'Generating lyrics for Robbie Williams (by word)...'
+    print generate_lyrics_word(data, 'Robbie Williams', "i don", 100)     
